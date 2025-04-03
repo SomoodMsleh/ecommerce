55470acdd51d29afd9025c49035f3c8cdcdf184f
SomoodMsleh <somoodmedwan@gmail.com>
2025-04-03 14:28:40 +0200

    structure for project

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import initApp from './src/index.router.js';
+import 'dotenv/config'
 const app = express();
 const port = process.env.PORT || 3000;
 
--- src/index.router.js
+++ src/index.router.js
@@ -1,5 +1,7 @@
+import cors from 'cors';
 const initApp = async(app,express)=>{
     app.use(express.json());
+    app.use(cors())
     app.get('/',(req,res)=>{
         return res.status(200).json({message:"Welcome ...."});
     });
