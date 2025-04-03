8a6d1bf56db1991423d060f66a997f12a8f818fa
SomoodMsleh <somoodmedwan@gmail.com>
2025-04-03 14:15:02 +0200

    initApp function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 import express from 'express';
+import initApp from './src/index.router.js';
+const app = express();
+const port = process.env.PORT || 3000;
+
+initApp(app,express);
+app.listen(port,()=>{
+    console.log(`server is running in port ${port} ....`);
+})
--- src/index.router.js
+++ src/index.router.js
@@ -1,3 +1,11 @@
-const initApp = async()=>{
-    
+const initApp = async(app,express)=>{
+    app.use(express.json());
+    app.get('/',(req,res)=>{
+        return res.status(200).json({message:"Welcome ...."});
+    });
+    app.use((req, res) => {
+        return res.status(404).json({ message: "page not found" });
+    });
 };
+
+export default initApp;
