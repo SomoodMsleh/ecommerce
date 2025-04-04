53cb7867d9e21a5cd32d1ce09b8fcc2915eda8d9
SomoodMsleh <somoodmedwan@gmail.com>
2025-04-04 13:50:39 +0200

    category Model

---
+++ DB/models/category.model.js
@@ -0,0 +1,34 @@
+import { object } from "joi";
+import mongoose, { Schema,Types,model } from "mongoose";
+
+const categorySchema = new Schema({
+    name:{
+        type:String,
+        required:true,
+        min:3,
+        max:50
+    },
+    slug:{
+        type:String,
+        required:true,
+    },
+    status:{
+        type:String,
+        enum:['active','not_active'],
+        default:'active'
+    },
+    image:{
+        type:object,
+    },
+    createdBy:{
+        type:Types.ObjectId,
+        ref:'User',
+    },
+    updateBy:{
+        type:Types.ObjectId,
+        ref:'User',
+    }
+},{timestamps:true});
+
+const categoryModel = mongoose.models.Category || model('Category',categorySchema);
+export default categoryModel;
