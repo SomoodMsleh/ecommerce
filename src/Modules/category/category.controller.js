f5faeffc850ae8f1f5b2781b92b6b7f1e5752ac6
SomoodMsleh <somoodmedwan@gmail.com>
2025-04-05 05:33:54 +0200

    edit for create category endpoint

--- src/Modules/category/category.controller.js
+++ src/Modules/category/category.controller.js
@@ -3,9 +3,10 @@ import slugify from 'slugify';
 
 export const createCategory = async (req,res,next)=>{
     const {name} = req.body;
-    const slug = slugify(name);
-    const createdBy = req.userId;
-    const category = await categoryModel.create({name,slug,createdBy})
+    req.body.slug = slugify(name);
+    req.body.createdBy = req.userId;
+    req.body.updateBy = req.userId;
+    const category = await categoryModel.create(req.body);
     return res.status(201).json({message:"successfully",category});
 };
 
