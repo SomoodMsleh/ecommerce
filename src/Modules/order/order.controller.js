8314d91f8534d6b67f032c296dc660bad713dfc1
SomoodMsleh <somoodmedwan@gmail.com>
2025-04-13 16:35:24 +0300

    create order endpoint part 2

--- src/Modules/order/order.controller.js
+++ src/Modules/order/order.controller.js
@@ -1,6 +1,7 @@
 import orderModel from "../../../DB/models/order.model.js";
 import cartModel from "../../../DB/models/cart.model.js";
-import couponModel from "../../../DB/models/coupon.model.js"
+import couponModel from "../../../DB/models/coupon.model.js";
+import productModel from "../../../DB/models/product.model.js"
 
 export const createOrder = async (req,res)=>{
     const {couponName} = req.body; 
@@ -19,6 +20,18 @@ export const createOrder = async (req,res)=>{
         if(coupon.usedBy.includes(req.userId)){
             return res.status(400).json({message:"this coupon already used"});
         }
+    }
+    const finalProducts = [];
+    for(let product of cart.products){
+        const checkProduct = await productModel.findOne({_id:product.productId,stock:{$gte:product.quantity}});
+        if(!checkProduct){
+            return res.status(400).json({message:"product quantity not available"});
+        }
+        // convert product from BSON to JSON used toObject()
+        product = product.toObject();
+        product.productName = checkProduct.name;
+        product.unitPrice = checkProduct.price;
+        return res.status(201).json({message:"successfully",product});
     }
     return res.status(201).json({message:"successfully",cart});
 };
